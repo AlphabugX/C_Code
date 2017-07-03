7b0644563db4af2220fc1cf9a818ccf1e7e0ea7b
NMTech0x90 <nmtech@aliyun.com>
2017-07-03 16:34:02 +0000

    Unauthorized

--- 0x02_helloworld/main.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-int main(void)
-{	
-	char name='A';
-	printf("Hello,%c\n",name);
-	return 0;
-}
--- 0x08_if/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(void)
-{
-	int x = 1;
-	if ( x != 0){
-		printf("x is nonzero.\n");
-	}
-	return 0;
-}
